b3b9f659416fe551d8efc58b7d0a12057be1b99d
SkylarSiggard <siggardskylar@gmail.com>
2023-03-27 08:22:21 -0600

    done

--- March 2023/march2023.ts
+++ March 2023/march2023.ts
@@ -335,8 +335,18 @@ export function setAlarmV2(employed: boolean, vacation: boolean) {
     return employed && !vacation
 }
 
-
-
+//? Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, 
+//? and return the largest and lowest number in that list, respectively.
+//* My Asnwer 
+export const minV1 = (list: number[]): number => {
+    return Math.min(...list)
+};
+  export const maxV1 = (list: number[]): number => {
+    return Math.max(...list)
+};
+//! Better Answer 
+export const min = list => Math.min(...list);
+export const max = list => Math.max(...list);
 
 
 
